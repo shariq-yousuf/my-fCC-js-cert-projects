32cd31190fd9ad217761aed0fefaa3b16998cd34
shariq-yousuf <shariqyousuf01@gmail.com>
2024-07-08 20:24:08 +0500

    add first project with js

---
+++ script.js
@@ -0,0 +1,33 @@
+const projects = [
+  {
+    id: 1,
+    title: "RPG - Dragon Repeller",
+    description:
+      "In this practice project, I have learned fundamental programming concepts in JavaScript by coding a Role Playing Game. I have learned to work with arrays, strings, objects, functions, loops, if/else statements, and more.",
+    imgSrc: "./src/RPG-Dragon-Repeller.png",
+    liveUrl: "./Projects/RPG-Dragon-Repeller/index.html",
+    githubLink:
+      "https://github.com/shariq-yousuf/my-fCC-js-cert-projects/tree/main/Projects/RPG-Dragon-Repeller",
+  },
+];
+
+const projectsDiv = document.querySelector("#projects-div");
+const reverseProjectsArr = projects.reverse();
+
+reverseProjectsArr.forEach((project) => {
+  projectsDiv.innerHTML += `
+    <div class="card">
+      <div class="bg">
+        <img src="${project.imgSrc}" alt="${project.title}" />
+        <h2 class="card-title">${project.title}</h2>
+        <div class="links-container">
+          <a href="${project.liveUrl}">
+          <i class="material-symbols-outlined live-icon live-icon">visibility</i>
+          </a>
+          <a href="${project.githubLink}"><i class="fa fa-github live-icon"></i></a>
+        </div>
+      </div>
+      <div class="blob"></div>
+    </div>
+  `;
+});
