12f76849a6229733d3f945c50a4d8d5cd2aff7e0
shariq-yousuf <shariqyousuf01@gmail.com>
2024-07-26 14:27:51 +0500

    chore: modifry styles and add error msg

--- Projects/Pokémon-Search-App/dist/script.js
+++ Projects/Pokémon-Search-App/dist/script.js
@@ -2,14 +2,26 @@
 const searchForm = document.querySelector("#search-form");
 const searchInput = document.querySelector("#search-input");
 const searchBtn = document.querySelector("#search-button");
+const errMsg = document.querySelector(".err-msg");
 const pokemonName = document.querySelector("#pokemon-name");
 const pokemonId = document.querySelector("#pokemon-id");
 const weightEl = document.querySelector("#weight");
 const heightEl = document.querySelector("#height");
 const imgContainer = document.querySelector(".img-container");
 const typesEl = document.querySelector("#types");
+const td2ndChilds = document.querySelectorAll("td:nth-child(2)");
 const url = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
+const resetData = {
+    name: "",
+    id: "",
+    weight: "",
+    height: "",
+    sprites: { front_default: "" },
+    types: [],
+    stats: [],
+};
 const getData = async () => {
+    errMsg.style.opacity = "0";
     const inputValue = searchInput.value
         .trim()
         .toLowerCase()
@@ -20,18 +32,22 @@ const getData = async () => {
         displayData(data);
     }
     catch (err) {
-        alert("Pokémon not found");
+        // alert("Pokémon not found");
+        errMsg.style.opacity = "1";
         console.error("Fetching error:", err);
+        displayData(resetData);
+        td2ndChilds.forEach((td) => (td.textContent = ""));
     }
+    searchInput.value = "";
 };
 const displayData = (data) => {
-    const { name, id, weight, height, sprites, types, stats } = data;
+    const { name, id, weight, height, sprites, types, stats, } = data;
     pokemonName.textContent = name.toUpperCase();
-    pokemonId.textContent = `#${id.toString()}`;
+    pokemonId.textContent = id ? `#${id.toString()}` : "";
     weightEl.textContent = weight.toString();
     heightEl.textContent = height.toString();
     imgContainer.innerHTML = `
-    <img src="${sprites.front_default}" alt="${name} sprite" id="sprite" />
+    <img src="${sprites.front_default}" id="sprite" />
   `;
     typesEl.innerHTML = "";
     types.forEach((type) => {
